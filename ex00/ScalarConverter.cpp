b998bd9e8961b041957c5925678d48a672799146
Thomas Parratt <tparratt@c2r2p2.hive.fi>
2024-11-08 16:50:09 +0200

    a few more things to handle

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -3,49 +3,80 @@
 
 void ScalarConverter::convert(std::string str)
 {
+    int     valuei = 0;
+    float   valuef = 0;
+    double  valued = 0;
 
-    // need to have a check for if string is a mixture of chars and ints
+    bool    i = false;
+    bool    f = false;
+    bool    d = false;
+
+    // need to have a check for if string is a mixture of chars and ints, or all chars // MORE THAN ONE CHAR or ONE CHAR NOT AT END
+    // nan
+    // inf
 
     if (str.find('.') != std::string::npos)
     {
-        if (str.find('f') == str.length() - 1) // literal type is float
+        if (str.find('f') == str.length() - 1)
+        {
             std::cout << "literal type is float" << std::endl;
+            f = true;
+            valuef = std::stof(str);
+        }
         else
+        {
             std::cout << "literal type is double (if it doesn't contain chars)" << std::endl;
+            d = true;
+            valued = std::stod(str);
+        }
     }
-    // else if (str.length() == 1 && std::isprint(str[0])) // this doesn't work
-    //     std::cout << "literal type is char" << std::endl;
     else
-        std::cout << "literal type is int or char" << std::endl; 
+    {
+        std::cout << "literal type is int or char" << std::endl;
+        i = true;
+        valuei = std::stoi(str); 
+    }
 
-    // if (std::stod(str) == true && )
-    //     double  value = std::stod(str);
-    // else if ()
-
-
-    // int     value = std::stoi(str);
-    // float   value = std::stof(str);
-    // double  value = std::stod(str);
-
-    // if (value == std::floor(value) && std::isprint(value))
-    // {
-    //     std::cout << "char: '" << static_cast<char>(value) << "'" << std::endl;
-    // }
-    // else
-    // {
-    //     std::cout << "char: Non displayable" << std::endl;
-    // }
-
-    // std::cout << "int: " << static_cast<int>(value) << std::endl;
-
-    // if (value == std::floor(value))
-    // {
-    //     std::cout << "float: " << static_cast<float>(value) << ".0f" << std::endl;
-    //     std::cout << "double: " << static_cast<double>(value) << ".0" << std::endl;
-    // }
-    // else
-    // {
-    //     std::cout << "float: " << static_cast<float>(value) << "f" << std::endl;
-    //     std::cout << "double: " << static_cast<double>(value) << std::endl;
-    // }
+    if (i)
+    {
+        if (std::isprint(valuei))
+            std::cout << "char: '" << static_cast<char>(valuei) << "'" << std::endl;
+        else
+            std::cout << "char: Non displayable" << std::endl;
+        std::cout << "int: " << valuei << std::endl;
+        std::cout << "float: " << static_cast<float>(valuei) << ".0f" << std::endl;
+        std::cout << "double: " << static_cast<double>(valuei) << ".0" << std::endl;
+    }
+    if (f)
+    {
+        if (valuef == std::floor(valuef) && std::isprint(valuef))
+            std::cout << "char: '" << static_cast<char>(valuef) << "'" << std::endl;
+        else
+            std::cout << "char: Non displayable" << std::endl;
+        std::cout << "int: " << static_cast<int>(valuef) << std::endl;
+        if (valuef == std::floor(valuef))
+            std::cout << "float: " << valuef << ".0f" << std::endl;
+        else
+            std::cout << "float: " << valuef << "f" << std::endl;
+        if (valuef == std::floor(valuef))
+            std::cout << "double: " << static_cast<double>(valuef) << ".0" << std::endl;
+        else
+            std::cout << "double: " << static_cast<double>(valuef) << std::endl;
+    }
+    if (d)
+    {
+        if (valued == std::floor(valuef) && std::isprint(valued))
+            std::cout << "char: '" << static_cast<char>(valued) << "'" << std::endl;
+        else
+            std::cout << "char: Non displayable" << std::endl;
+        std::cout << "int: " << static_cast<int>(valued) << std::endl;
+        if (valued == std::floor(valued))
+            std::cout << "float: " << static_cast<float>(valued) << ".0f" << std::endl;
+        else
+            std::cout << "float: " << static_cast<float>(valued) << "f" << std::endl;
+        if (valued == std::floor(valued))
+            std::cout << "double: " << valued << ".0" << std::endl;
+        else
+            std::cout << "double: " << valued << std::endl;
+    }
 }
