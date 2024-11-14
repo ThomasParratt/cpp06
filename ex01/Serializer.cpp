2726b1a6afc810098e7e7ad82ec5480c7b13a9b1
Thomas Parratt <tparratt@c2r2p2.hive.fi>
2024-11-14 11:33:00 +0200

    deserializer

--- ex01/Serializer.cpp
+++ ex01/Serializer.cpp
@@ -5,7 +5,7 @@ uintptr_t    Serializer::serialize(Data* ptr)
     return (reinterpret_cast<uintptr_t>(ptr->getValue()));
 }
 
-/*static Data*        deserialize(uintptr_t raw)
+Data*        Serializer::deserialize(uintptr_t raw)
 {
-    
-}*/
+    return (reinterpret_cast<Data*>(raw));
+}
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -13,4 +13,8 @@ int main(void)
 
     std::cout << ptr_value << std::endl;
     std::cout << std::showbase << std::hex << ptr_value << std::endl;
+
+    Data    *data_new = Serializer::deserialize(ptr_value);
+
+    std::cout << data_new << std::endl;
 }
