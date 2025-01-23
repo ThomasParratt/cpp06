853a206f3c7f5e9d78f3c6149fd52d4fbd3369f1
Thomas Parratt <tparratt@c1r3p7.hive.fi>
2025-01-23 13:31:50 +0200

    Data is now struct

--- ex01/Serializer.hpp
+++ ex01/Serializer.hpp
@@ -1,5 +1,4 @@
-#ifndef SERIALIZER_HPP
-# define SERIALIZER_HPP
+#pragma once
 
 # include <iostream>
 # include <cstdint>
@@ -17,5 +16,3 @@ class Serializer
         static uintptr_t    serialize(Data* ptr);
         static Data*        deserialize(uintptr_t raw);
 };
-
-#endif
